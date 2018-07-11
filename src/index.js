aa9495c4d35242d97ef3d8751a70a9dea6816675
Zhonglu <wangzhonglu@gmail.com>
2018-07-10 22:39:48 -0400

    Add vertical gauge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Circle from './circle.js';
 import Rect from './Rect.js';
 import Color from './Color.js';
+import VerticalGauge from './veritical-gauge';
 
-export { Circle, Rect, Color};
+export { Circle, Rect, Color, VerticalGauge};
---
+++ src/veritical-gauge.js
@@ -0,0 +1,28 @@
+import BaseCanvas from './base-canvas';
+
+export default class VerticalGauge extends BaseCanvas {
+
+  constructor(canvas) {
+    super(canvas);
+    this._a = 0;
+  }
+
+  drawRect(i) {
+    const x = 10;
+    let y = i * 15 + 10;
+
+    this._ctx.fillStyle = 'green';
+    this._ctx.fillRect(x, y, 100, 10);
+  }
+
+  draw() {
+    this._timer = setInterval(() => {
+      this.drawRect(this._a);
+      this._a += 1;
+      if (this._a > 10) {
+        this._a = 0;
+        this.clearAll();
+      }
+    }, 100);
+  }
+}
